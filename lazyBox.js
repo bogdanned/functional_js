1ab37da70cb71364fada6fc26993fe53a175278e
Bogdan Nedelcu <bogdan.nedelcu@lokalleads.de>
2018-01-04 09:34:14 +0100

    lazy box

--- lazyBox.js
+++ lazyBox.js
@@ -1,11 +1,10 @@
-const Task = require('data').task
+const Task = require('data.task')
 
 const Box = x => ({ 
     map: f => Box(f(x)),
     fold: f => f(x),
     inspect: () => `Box(${x})`
 })
-
 
 const LazyBox = g => ({ 
     map: f => LazyBox(f(g())),
@@ -15,5 +14,13 @@ const LazyBox = g => ({
 
 //Capture Side Effects in a Task
 
+const lauchMissiles = () => new Task((rej, res) => {
+    console.log("launch missiles!")
+    res("missile")
+}) 
 
+//fork is called to execute the buildt function
 
+lauchMissiles()
+.map(x => x + 1)
+.fork( e => console.log('err: ', e), x => console.log('success: ', x))
